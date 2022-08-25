e717264fff640d421ddf48e6ffc7d368ba2c301c
Savaş Dede <svsdd22@gmail.com>
2022-08-25 20:28:33 +0300

    jaxws using to generate wsdl class

---
+++ src/main/java/com/wsdl/JavaWSDL/JavaWsdlApplication.java
@@ -0,0 +1,13 @@
+package com.wsdl.JavaWSDL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWsdlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWsdlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsdl/JavaWSDL/JavaWsdlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsdl.JavaWSDL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWsdlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
